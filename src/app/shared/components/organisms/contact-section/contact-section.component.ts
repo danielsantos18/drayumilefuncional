570214c897ace2cdb8d3f7243a6453f42b8924c8
danielsantos18 <danielsntos06@gmail.com>
2026-09-17 18:16:58 -0500

    actualizacion drayumilefuncional

--- src/app/shared/components/organisms/contact-section/contact-section.component.ts
+++ src/app/shared/components/organisms/contact-section/contact-section.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { SectionTitleComponent } from '../../atoms/section-title/section-title.component';
 import { ContactInfoItemComponent } from '../../molecules/contact-info-item/contact-info-item.component';
 import { SocialGroupComponent } from '../../molecules/social-group/social-group.component';
-import { ContactFormComponent, ContactFormData } from '../../molecules/contact-form/contact-form.component';
 
 @Component({
   selector: 'app-contact-section',
@@ -12,15 +11,10 @@ import { ContactFormComponent, ContactFormData } from '../../molecules/contact-f
     CommonModule,
     SectionTitleComponent,
     ContactInfoItemComponent,
-    SocialGroupComponent,
-    ContactFormComponent
+    SocialGroupComponent
   ],
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.scss'
 })
-export class ContactSectionComponent {
-  onFormSubmitted(data: ContactFormData): void {
-    console.log('Formulario enviado:', data);
-  }
-}
+export class ContactSectionComponent {}
 
